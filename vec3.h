523d978db7dad2eede0308832fb41a90c74e5e10
Ryomanba <datemanba3@gmail.com>
2021-11-20 19:43:24 +0900

    update

---
+++ vec3.h
@@ -0,0 +1,23 @@
+#ifndef VEC3_H
+# define VEC3_H
+
+typedef struct	vec3
+{
+	double x;
+	double y;
+	double z;
+} t_vec3;
+
+t_vec3	vec3_add(t_vec3 u, t_vec3 v);
+t_vec3	vec3_sub(t_vec3 u, t_vec3 v);
+t_vec3	vec3_mul(t_vec3 u, t_vec3 v);
+double	vec3_dot(t_vec3 u, t_vec3 v);
+t_vec3	vec3_cross(t_vec3 u, t_vec3 v);
+
+double	vec3_length_squared(t_vec3 v);
+double	vec3_length(t_vec3 v);
+t_vec3	vec3_mul_double(t_vec3 u, double t);
+t_vec3	vec3_div(t_vec3 v, double t);
+t_vec3	unit_vector(t_vec3 v);
+
+#endif
---
+++ vec3_operator.c
@@ -0,0 +1,46 @@
+#include "vec3.h"
+
+t_vec3  vec3_add(t_vec3 u, t_vec3 v)
+{
+	t_vec3	add;
+
+	add.x = u.x + v.x;
+	add.y = u.y + v.y;
+	add.z = u.z + v.z;
+	return (add);
+}
+
+t_vec3  vec3_sub(t_vec3 u, t_vec3 v)
+{
+	t_vec3	sub;
+
+	sub.x = u.x - v.x;
+	sub.y = u.y - v.y;
+	sub.z = u.z - v.z;
+	return (sub);
+}
+
+t_vec3  vec3_mul(t_vec3 u, t_vec3 v)
+{
+	t_vec3	mul;
+
+	mul.x = u.x * v.x;
+	mul.y = u.y * v.y;
+	mul.z = u.z * v.z;
+	return (mul);
+}
+
+double  vec3_dot(t_vec3 u, t_vec3 v)
+{
+	return ((u.x * v.x) + (u.y * v.y) + (u.z * v.z));
+}
+
+t_vec3	vec3_cross(t_vec3 u, t_vec3 v)
+{
+	t_vec3	cross;
+
+	cross.x = u.y * v.z - u.z * v.y;
+	cross.y = u.z * v.x - u.x * v.z;
+	cross.z = u.x * v.y - u.y * v.x;
+	return (cross);
+}
