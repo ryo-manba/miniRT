a5c6af2798c71721bba6f9fbaa42081c44890ab3
Kawada, Yosuke <benefium@gmail.com>
2021-12-07 19:59:17 +0900

    Revert "WIP: カメラ"

--- mr_camera.h
+++ mr_camera.h
@@ -1,13 +1,11 @@
 #ifndef MR_CAMERA_H
 # define MR_CAMERA_H
 
-typedef struct s_viewport
+typedef struct s_camerax
 {
-	t_camera	camera;
-	double		vp_height;
-	double		vp_width;
-	double		focal_length;
-	t_vec3		screen_center;
-}	t_viewport;
+	double	vp_height;
+	double	vp_width;
+	double	focal_length;
+}	t_camerax;
 
 #endif
